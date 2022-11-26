4327a27e88cd024f0ca726f2b74f2a6c00c1f8ad
Liza Ermakovich <110353775+privetyaliza@users.noreply.github.com>
2022-11-26 15:20:34 +0200

    Add files via upload

---
+++ dance.py
@@ -0,0 +1,13 @@
+"""
+COMP.CS.100 Programming 1
+Code template for tanssipelit assigment.
+"""
+
+def calculate_average(s):
+
+    '''fghjk fgyouhl gog
+    hguo oftyguh gswyuto otrty
+    hyb;ogtfhjk'''
+    av = sum(s.values())/len(s)
+    return av
+
